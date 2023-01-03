cd5e49e6350065b0d6ded7434903154e76c39f7e
Bedirhan Harputuloglu <bedirhanharputlu@gmail.com>
2023-01-03 20:26:29 +0300

    components eklendi todo-mvc  iskelet oluşturuldu.

---
+++ src/components/Todo/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Header from './Header'
+import Main from './Main'
+import Footer from './Footer'
+
+function Todos() {
+  return (
+    <div class="todoapp">
+        <Header/>
+        <Main/>
+        <Footer/>
+        <footer class="info">
+            <p>Click to edit a todo</p>
+            <p>Created by <a href="https://d12n.me/">Dmitry Sharabin</a></p>
+            <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
+        </footer>
+    </div>
+  )
+}
+
+export default Todos
